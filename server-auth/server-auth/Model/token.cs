a498f7b1d26a194e47929ec64e9c47d31ba1f0f3
MugenZeta <hgarner213@gmail.com>
2023-10-23 23:17:24 -0500

    .

---
+++ server-auth/server-auth/Model/token.cs
@@ -0,0 +1,12 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Model
+{
+
+    public class token
+    {
+        private bool isActive = false;
+        private Timer tokenStaleTimer;
+    }
+}
